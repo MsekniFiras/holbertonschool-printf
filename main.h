d5bfab8751f9247899a52324cdd02c2bb6a6f94c
MsekniFiras <msekni.firas@gmail.com>
2023-05-05 12:40:46 -0700

    m

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
 int _printf(const char *format, ...);
 
 #endif
