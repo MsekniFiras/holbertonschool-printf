77ec5f40691c5628f1669722fd18f10bdde6aff3
MsekniFiras <msekni.firas@gmail.com>
2023-05-05 12:39:05 -0700

    m

--- _printf.c
+++ _printf.c
@@ -1,40 +1,39 @@
 #include <stdarg.h>
 #include <stdio.h>
-
-int _printf(const char format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    int count = 0;
-
-    for (const charp = format; p; p++) {
-        if (p != '%') {
-            putchar(p);
-            count++;
-            continue;
-        }
-
-        switch ((++p)) {
-            case 'c':
-                putchar(va_arg(args, int));
-                count++;
-                break;
-            case 's':
-                for (const char s = va_arg(args, const char); s; s++) {
-                    putchar(s);
-                    count++;
-                }
-                break;
-            case '%':
-                putchar('%');
-                count++;
-                break;
-            default:
-                // unsupported conversion specifier
-                break;
-        }
-    }
-
-    va_end(args);
-    return count;
+int _printf(const char format, ...)
+{
+va_list args;
+va_start(args, format);
+int count = 0;
+for (const charp = format; p; p++)
+{
+if (p != '%')
+{
+putchar(p);
+count++;
+continue;
+}
+switch ((++p))
+{
+case 'c':
+putchar(va_arg(args, int));
+count++;
+break;
+case 's':
+for (const char s = va_arg(args, const char); s; s++)
+{
+putchar(s);
+count++;
+}
+break;
+case '%':
+putchar('%');
+count++;
+break;
+default:
+break;
+}
+}
+va_end(args);
+return (count);
 }
