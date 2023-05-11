c467dc2ce2447e72960df703300cfa2020aa44c2
MsekniFiras <msekni.firas@gmail.com>
2023-05-11 05:13:15 -0700

    get function type

---
+++ get_function.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * get_function - determine which print function to use.
+ *
+ * @C: The character that identifies the type of the variable to print.
+ *
+ * Return: pointer to the matching print function.
+ */
+
+int (*get_function(const char *C))(va_list)
+{
+	int i;
+		holberton_t types[] = {
+			{"s", print_s},
+			{"c", print_c},
+			{"d", print_d},
+			{"i", print_i},
+			{"%", print_100},
+			{NULL, NULL},
+		};
+		for (i = 0; types[i].X; i++)
+		{
+			if (*C == types[i].X[0])
+				return (types[i].f);
+		}
+		return (NULL);
+}
