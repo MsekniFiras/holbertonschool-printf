94a3b7930f276ad7ecb59d52d94b15f154c3f2cc
MsekniFiras <msekni.firas@gmail.com>
2023-05-11 05:47:29 -0700

    p%

---
+++ print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_percent - prints percent.
+ * @valley: arg
+ * Return: 1
+ */
+
+int print_100(va_list valley)
+{
+	(void)valley;
+	_putchar('%');
+	return (1);
+}
